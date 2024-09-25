35d9c814c54f5fa16f451d3514d1c212906abd9f
darioalejo <alejandrolavayen10@gmail.com>
2024-09-25 17:44:39 -0500

    Se agrega el proyecto

---
+++ js/main.js
@@ -0,0 +1,45 @@
+jQuery(window).on('load', function() {
+    "use strict";
+    // HIDE PRELOADER
+    $(".preloader").addClass("hide-preloader");   
+    
+    // SHOW/ANIMATE ANIMATION CONTAINER
+    setTimeout(function(){
+        $("#intro .animation-container").each(function() {
+            var e = $(this);
+            setTimeout(function(){
+                e.addClass("run-animation");
+            }, e.data("animation-delay"));
+        });
+    }, 700);
+});
+
+jQuery(document).ready(function($) {
+    "use strict";
+
+    // SMOOTH SCROLL FOR SAME PAGE LINKS
+    $(document).on('click', 'a.smooth-scroll', function(event) {
+        var target = $.attr(this, 'href');
+        if ($(target).length) {
+            event.preventDefault(); // Prevenir solo si el objetivo existe
+            $('html, body').animate({
+                scrollTop: $(target).offset().top - 80
+            }, 500);
+        }
+    });
+    
+    // SCROLL REVEAL SETUP
+    window.sr = ScrollReveal();
+    sr.reveal(".scroll-animated-from-right", { 
+        duration: 600,
+        delay: 0,
+        origin: "right",
+        rotate: { x: 0, y: 0, z: 0 },
+        opacity: 0,
+        distance: "20vh",
+        viewFactor: 0.4,
+        scale: 1,
+    });
+    
+
+});
